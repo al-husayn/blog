eaf090adee821d42c8005f5ad446661a8498472b
Al-Hussein Abubakar <alhusayn532@gmail.com>
2026-06-07 23:46:27 +0000

    chore: refactor ESLint configuration for improved clarity and structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,19 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-    {
-        ignores: ['.next/**', '.source/**', 'node_modules/**'],
-    },
-    ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    eslintConfigPrettier,
+    globalIgnores(['.next/**', '.source/**', 'node_modules/**', '**/*.md', '**/*.mdx', 'scripts/generate-read-times.js']),
     {
         files: ['scripts/**/*.js'],
         rules: {
             '@typescript-eslint/no-require-imports': 'off',
         },
     },
-];
+]);
 
 export default eslintConfig;
